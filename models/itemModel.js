df079f02fa321a0a8b64287c4b1c24a9015a4665
Jose Bernard Sedalo <sedalojosebernard@gmail.com>
2024-08-26 01:18:13 -0400

    first push

---
+++ models/itemModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+        
+    },
+    quantity: {
+        type: Number,
+        min: 0,
+        required: true
+    }, 
+    rank: {
+        type: Number,
+        default: 0
+    }
+
+    
+})
+const itemModel = mongoose.models.item || mongoose.model("item", itemSchema)
+
+export default itemModel;
+
