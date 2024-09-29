bb86fb27ee39dfb48e0e0357727298de0bade80e
halomie <156081172+JoseBernardS@users.noreply.github.com>
2024-09-29 14:40:31 -0400

    Update orderRoute.js

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,16 +1,21 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import { placeOrder,verifyOrder,userOrders,getOrders,updateStatus,getAddress } from "../controllers/orderController.js";
-
+import { 
+    initiateCheckout,
+    handleWebhook,
+    userOrders,
+    getOrders,
+    updateStatus,
+    getAddress 
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
-orderRouter.post("/placeorder", authMiddleware, placeOrder);
-orderRouter.post("/verify",verifyOrder)
-orderRouter.post("/user-orders",authMiddleware,userOrders)
-orderRouter.get("/list-orders",getOrders)
-orderRouter.post("/update-status",updateStatus)
-orderRouter.get("/get-address",authMiddleware,getAddress)
-
+orderRouter.post("/initiate-checkout", authMiddleware, initiateCheckout);
+orderRouter.post("/webhook", express.raw({type: 'application/json'}), handleWebhook);
+orderRouter.get("/user-orders", authMiddleware, userOrders);
+orderRouter.get("/list-orders", getOrders);
+orderRouter.post("/update-status", updateStatus);
+orderRouter.get("/get-address", authMiddleware, getAddress);
 
 export default orderRouter;
